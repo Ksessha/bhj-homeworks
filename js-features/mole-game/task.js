bee1ab8c4a11b5eb6cd5b6ee30ca97a0029df4a5
Ksesha <ksusha.kinsky@gmail.com>
2025-03-29 22:29:44 +0300

    Update

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,34 +3,60 @@ const lostCounter = document.getElementById('lost');
 
 let dead = 0;
 let lost = 0;
+let gameInterval;
 
-function getHole(index){
+function getHole(index) {
     return document.getElementById(`hole${index}`);
 }
 
-function checkGameStatus(){
-    if(dead>=10){
+function checkGameStatus() {
+    if (dead >= 10) {
+        clearInterval(gameInterval);
         alert('Победа! Вы убили 10 кротов!');
         resetGame();
-    }
-    else if(lost>=5){
+    } else if (lost >= 5) {
+        clearInterval(gameInterval);
         alert('Поражение! Слишком много промахов!');
         resetGame();
     }
 }
 
-function resetGame(){
+function resetGame() {
     dead = 0;
     lost = 0;
     deadCounter.textContent = dead;
     lostCounter.textContent = lost;
+    for (let i = 1; i <= 9; i++) {
+        getHole(i).classList.remove('hole_has-mole');
+    }
+
+    startGame();
 }
+
+
+function activateRandomHole() {
+
+    for (let i = 1; i <= 9; i++) {
+        getHole(i).classList.remove('hole_has-mole');
+    }
+    
+    const randomIndex = Math.floor(Math.random() * 9) + 1;
+    getHole(randomIndex).classList.add('hole_has-mole');
+}
+
+function startGame() {
+    if (gameInterval) {
+        clearInterval(gameInterval);
+    }
+
+    gameInterval = setInterval(activateRandomHole, 800);
+}
+
 for (let i = 1; i <= 9; i++) {
     const hole = getHole(i);
     
     hole.addEventListener('click', function() {
         if (hole.classList.contains('hole_has-mole')) {
-
             dead++;
             deadCounter.textContent = dead;
             hole.classList.remove('hole_has-mole');
@@ -38,18 +64,10 @@ for (let i = 1; i <= 9; i++) {
             lost++;
             lostCounter.textContent = lost;
         }
-
+        
         checkGameStatus();
     });
 }
 
-function activateRandomHole() {
-    for (let i = 1; i <= 9; i++) {
-        getHole(i).classList.remove('hole_has-mole');
-    }
 
-    const randomIndex = Math.floor(Math.random() * 9) + 1;
-    getHole(randomIndex).classList.add('hole_has-mole');
-}
-
-setInterval(activateRandomHole, 800);
+startGame();
