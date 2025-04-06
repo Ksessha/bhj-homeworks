c3ab98347452828aa2b97469554ee1bd1fa4b459
Ksesha <ksusha.kinsky@gmail.com>
2025-04-06 23:27:09 +0300

    Update

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,20 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const fontSizeControls = document.querySelectorAll('.font-size');
-    const bookElement = document.getElementById('book');
-    
-    fontSizeControls.forEach(control => {
-        control.addEventListener('click', function(e) {
-            e.preventDefault();
-            fontSizeControls.forEach(item => {
-                item.classList.remove('font-size_active');
-            });
-            this.classList.add('font-size_active');
-            const size = this.dataset.size;
-            if (size === 'small') {
-                bookElement.classList.add('book_fs-small');
-            } else if (size === 'big') {
-                bookElement.classList.add('book_fs-big');
+    function handleFontSizeChange(e) {
+        e.preventDefault();
+        
+        const bookElement = document.getElementById('book');
+        const fontSizeControls = document.querySelectorAll('.font-size');
+        fontSizeControls.forEach(item => {
+            item.classList.remove('font-size_active');
+        });
+        this.classList.add('font-size_active');
+        const size = this.dataset.size;
+        bookElement.classList.remove('book_fs-small', 'book_fs-big');
+        if (size === 'small') {
+            bookElement.classList.add('book_fs-small');
+        } else if (size === 'big') {
+            bookElement.classList.add('book_fs-big');
+        }
+    }
+
+    function initFontSizeControls() {
+        const fontSizeControls = document.querySelectorAll('.font-size');
+        
+        fontSizeControls.forEach(control => {
+            control.removeEventListener('click', handleFontSizeChange);
+            control.addEventListener('click', handleFontSizeChange);
+        });
+    }
+
+    initFontSizeControls();
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes.length) {
+                initFontSizeControls();
             }
         });
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
     });
 });
